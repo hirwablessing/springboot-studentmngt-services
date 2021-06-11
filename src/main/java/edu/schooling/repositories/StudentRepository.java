097fffd1f59fa49b637ad19b80c17780fb2a006c
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-06-11 09:27:47 +0200

    added custom method

--- src/main/java/edu/schooling/repositories/StudentRepository.java
+++ src/main/java/edu/schooling/repositories/StudentRepository.java
@@ -4,6 +4,9 @@ import edu.schooling.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    List<Student> findByFirstName(String firstName);
 }
